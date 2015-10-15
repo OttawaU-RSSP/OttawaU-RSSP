04176a5e16a312b96577796c4c91f2a775b58c8e
Gary Mardell <gary@shopify.com>
2015-10-15 17:37:53 -0400

    Add lawyer method.

--- app/models/lawyer.rb
+++ app/models/lawyer.rb
@@ -1,4 +1,8 @@
 class Lawyer < User
   has_many :assignees, foreign_key: :user_id
   has_many :applications, through: :assignees
+
+  def lawyer?
+    true
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   include Clearance::User
 
   validates :name, presence: true
+
+  def lawyer?
+    false
+  end
 end
