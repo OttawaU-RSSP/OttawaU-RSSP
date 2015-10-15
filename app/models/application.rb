bc6710c8a18c377702c59483f7dfc2bc45bc1014
Gary Mardell <gary@shopify.com>
2015-10-15 18:14:43 +0000

    Add transitions to application between states

--- app/models/application.rb
+++ app/models/application.rb
@@ -3,9 +3,30 @@ class Application < ActiveRecord::Base
 
   aasm column: :state do
     state :intake, initial: true
-    state :completed_intake
     state :follow_up
+    state :in_progress
     state :in_review
+    state :submitted
     state :accepted
+
+    event :intake_completed do
+      transitions from: :intake, to: :follow_up
+    end
+
+    event :follow_up_completed do
+      transitions from: :follow_up, to: :in_progress
+    end
+
+    event :review do
+      transitions from: :in_progress, to: :in_review
+    end
+
+    event :submitted do
+      transitions from: :in_review, to: :submitted
+    end
+
+    event :accepted do
+      transitions from: :submitted, to: :accepted
+    end
   end
 end
--- test/models/application_test.rb
+++ test/models/application_test.rb
@@ -6,4 +6,39 @@ class ApplicationTest < ActiveSupport::TestCase
 
     assert application.intake?
   end
+
+  test "application intake_completed transitions from intake to follow_up" do
+    application = Application.new
+    application.intake_completed
+
+    assert application.follow_up?
+  end
+
+  test "application follow_up_completed transitions from follow_up to in_progress" do
+    application = Application.new(state: 'follow_up')
+    application.follow_up_completed
+
+    assert application.in_progress?
+  end
+
+  test "application review transitions from in_progress to in_review" do
+    application = Application.new(state: 'in_progress')
+    application.review
+
+    assert application.in_review?
+  end
+
+  test "application submitted transitions from in_review to submitted" do
+    application = Application.new(state: 'in_review')
+    application.submitted
+
+    assert application.submitted?
+  end
+
+  test "application accepted transitions from submitted to accepted" do
+    application = Application.new(state: 'submitted')
+    application.accepted
+
+    assert application.accepted?
+  end
 end
