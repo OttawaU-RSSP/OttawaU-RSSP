99383ac55da603558a08c697cc0ce1da06d69ad6
Kaley Bibic <kaley.bibic@shopify.com>
2015-10-15 16:04:02 -0400

    * Add model file

--- db/migrate/20151015190030_create_sponsor_group.rb
+++ db/migrate/20151015190030_create_sponsor_group.rb
@@ -2,16 +2,25 @@ class CreateSponsorGroup < ActiveRecord::Migration
   def change
     create_table :sponsor_groups do |t|
       t.string :name
-      t.text :address
+      t.string :address_line_1
+      t.string :address_line_2
+      t.string :city
+      t.string :postal_code
       t.string :phone
       t.string :email
       t.string :citizenship_status
-      t.string :refugee_connection
+
+      t.boolean :connected_to_refugee
+      t.string :refugee_connection_type
+      t.boolean :refugee_outside_country_of_origin
+
       t.integer :group_size
-      t.boolean :SAH_connection
+      t.boolean :sah_connection
       t.boolean :interpreter_needed
       t.boolean :sufficient_resources
       t.boolean :criminal_record
+
+      t.timestamps
     end
   end
 end
