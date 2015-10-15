d750589b067828daf383e256a166df30bf1524d3
Gary Mardell <gary@shopify.com>
2015-10-15 17:33:33 -0400

    Remove unused methods.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,5 @@
 class User < ActiveRecord::Base
   include Clearance::User
 
-  ROLES = %w(lawyer admin)
-
   validates :name, presence: true
-
-  ROLES.each do |role|
-    define_method "#{role}?" do
-      type == role.capitalize
-    end
-  end
 end
