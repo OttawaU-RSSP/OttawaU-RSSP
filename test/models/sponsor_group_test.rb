ce0308a672728c71d34f518dd097748c4f5d7db2
Kirsten Westeinde <kirsten.westeinde@shopify.com>
2015-10-16 16:42:33 +0000

    Make the hound happy

--- test/models/sponsor_group_test.rb
+++ test/models/sponsor_group_test.rb
@@ -1,12 +1,12 @@
 require 'test_helper'
 
 class SponsorGroupTest < ActiveSupport::TestCase
-  test 'creating a sponsor group creates an associated application' do
+  test "creating a sponsor group creates an associated application" do
     sponsor_group = create_sponsor
     assert_not_nil sponsor_group.application
   end
 
-  test 'creating a sponsor group emails admin' do
+  test "creating a sponsor group emails admin" do
     SponsorGroupMailer.expects(:intake_form_submitted).returns(stub(:deliver_now))
 
     create_sponsor
@@ -15,6 +15,6 @@ class SponsorGroupTest < ActiveSupport::TestCase
   private
 
   def create_sponsor
-    SponsorGroup.create(name: 'name', email: 'email', phone: 'phone')
+    SponsorGroup.create(name: "name", email: "email", phone: "phone")
   end
 end
