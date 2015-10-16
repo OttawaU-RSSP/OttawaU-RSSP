4d8beb894809bb57f21e9bc7f96bc262f71573ae
Gary Mardell <gary@shopify.com>
2015-10-15 20:22:14 -0400

    Add tests for admin controller.

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
+Rails.application.routes.disable_clear_and_finalize = true
+Rails.application.routes.draw do
+  resources :admin_controller_test do
+    get :index, on: :collection
+  end
+end
+
+class AdminControllerTestController < ::AdminController
+  def index
+    head :ok
+  end
+end
+
 class AdminControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  tests AdminControllerTestController
+
+  test "must be logged in" do
+    get :index
+
+    assert_redirected_to new_session_path
+  end
+
+  test "must be an admin" do
+    sign_in_as(users(:lawyer))
+
+    get :index
+
+    assert_redirected_to new_session_path
+  end
 end
