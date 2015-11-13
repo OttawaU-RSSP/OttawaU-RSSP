a49accc6065d4ad5752c3656fe484d502e94b6b0
Kirsten Westeinde <kirsten.westeinde@shopify.com>
2015-11-13 02:05:19 +0000

    Addresses some of the suggested content tweaks

--- test/controllers/legal_internal/follow_up_call_forms_controller_test.rb
+++ test/controllers/legal_internal/follow_up_call_forms_controller_test.rb
@@ -27,7 +27,6 @@ class LegalInternal::FollowUpCallFormsControllerTest < ActionController::TestCas
     assert_equal attributes[:public_notes], sponsor_group.public_notes
     assert_equal attributes[:private_notes], sponsor_group.private_notes
     assert_equal true, sponsor_group.proper_group_size
-    assert_equal attributes[:refugee_name], sponsor_group.refugee_name
     assert_equal attributes[:refugee_nationality], sponsor_group.refugee_nationality
     assert_equal attributes[:refugee_current_location], sponsor_group.refugee_current_location
     assert_equal false, sponsor_group.connect_refugee_family_in_canada
@@ -45,7 +44,6 @@ class LegalInternal::FollowUpCallFormsControllerTest < ActionController::TestCas
       public_notes: 'public notes',
       private_notes: 'private notes',
       proper_group_size: '1',
-      refugee_name: 'Name',
       refugee_nationality: 'Nationality',
       refugee_current_location: 'Location',
       connect_refugee_family_in_canada: '0',
